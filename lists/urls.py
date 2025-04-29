dd1586c30d49d676ad2f09ed99bbdc6b302cd8f3
John Mathena <jmathena@2u.com>
2025-04-29 11:29:14 -0400

    get tests and migrations working for chapter 8

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.conf.urls import url
 from lists import views
 
 urlpatterns = [
-    path("new", views.new_list, name="new_list"),
-    path("<int:list_id>/", views.view_list, name="view_list"),
-    path("<int:list_id>/add_item", views.add_item, name="add_item"),
+    url(r"^new$", views.new_list, name="new_list"),
+    url(r"^(\d+)/$", views.view_list, name="view_list"),
+    url(r"^(\d+)/add_item$", views.add_item, name="add_item"),
 ]
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,7 +1,8 @@
-from django.urls import include, path
+from django.conf.urls import include, url
 from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    path("", list_views.home_page, name="home"),
-    path("lists/", include("lists.urls")),
+    url(r"^$", list_views.home_page, name="home"),
+    url(r"^lists/", include("lists.urls")),
 ]
