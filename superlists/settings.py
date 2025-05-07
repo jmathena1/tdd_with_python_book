84f887cf226564bfcb63f56781c7a80748245360
John Mathena <jmathena@2u.com>
2025-05-07 15:35:56 -0400

    add the hack

--- superlists/settings.py
+++ superlists/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-d19a5nlh!%q-xxh6#guvn+=te1rns)o^yj2v+3_mgehdav2srv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
