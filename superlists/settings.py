1001506881faa9e2711fdfc757f19ae6e5deaae5
John Mathena <jmathena@2u.com>
2025-05-16 12:07:47 -0400

    fix typo in secret key variable name

--- superlists/settings.py
+++ superlists/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     ALLOWED_HOSTS =[os.environ['SITENAME']]
 else:
     DEBUG = TRUE
