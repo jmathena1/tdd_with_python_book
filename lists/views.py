4ccaa708c35941b87d2f216ff76d73899096eef1
John Mathena <jmathena@2u.com>
2025-05-30 14:41:18 -0400

    Adjust new list view to do model validation

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from lists.models import Item, List
 
@@ -7,17 +8,24 @@ def home_page(request):
 
 
 def new_list(request):
-    nulist = List.objects.create()
-    Item.objects.create(text=request.POST["item_text"], list=nulist)
-    return redirect(f"/lists/{nulist.id}/")
+    list_ = List.objects.create()
+    item = Item.objects.create(text=request.POST["item_text"], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
+    return redirect(f"/lists/{list_.id}/")
 
 
 def view_list(request, list_id):
-    our_list = List.objects.get(id=list_id)
-    return render(request, "list.html", {"list": our_list})
+    list_ = List.objects.get(id=list_id)
+    return render(request, "list.html", {"list": list_})
 
 
 def add_item(request, list_id):
-    our_list = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST["item_text"], list=our_list)
-    return redirect(f"/lists/{our_list.id}/")
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list_)
+    return redirect(f"/lists/{list_.id}/")
--- superlists/urls.py
+++ superlists/urls.py
@@ -4,5 +4,5 @@ from lists import urls as list_urls
 
 urlpatterns = [
     url(r"^$", list_views.home_page, name="home"),
-    url(r"^lists/", include("lists.urls")),
+    url(r"^lists/", include(list_urls)),
 ]
